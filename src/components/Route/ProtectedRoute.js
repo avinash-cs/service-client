6ad60932ff2a5a09362c1c5d2403351a1503f8aa
avinash-cs <avinashsingh880086@gmail.com>
2022-12-10 15:39:35 +0530

    Avd

--- src/components/Route/ProtectedRoute.js
+++ src/components/Route/ProtectedRoute.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import {Redirect} from 'react-router-dom';
+import { Navigate } from "react-router";
 
 
 const ProtectedRoute = ({ component: Component }) => {
   const { isAuthenticated, loading } = useSelector((state) => state.user);
   if (loading === undefined || loading === true) return <></>;
-  return isAuthenticated === true ? <Component /> : <Redirect to="/login" />;
+  return isAuthenticated === true ? <Component /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
